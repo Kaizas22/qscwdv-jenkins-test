4321992fb05aed6ea47c7668061890e0940ffea4
Kaizas22 <kaizas22@gmail.com>
2019-04-01 19:51:02 +0200

    Update repository.groovy

--- repository.groovy
+++ repository.groovy
@@ -2,6 +2,7 @@ REPO_URL = "https://github.com/Kaizas22/"
 
 def checkout(repository) {
     def repos = "${REPO_URL}" + "${repository}" + ".git"
+    echo "${repos}"
     
     checkout scm: [
         $class: 'GitSCM',
