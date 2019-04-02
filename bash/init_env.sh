b41381eb8704450c97870d41dfdd624e390d1356
Kaizas22 <kaizas22@gmail.com>
2019-04-02 22:02:45 +0200

    Create init_env.sh

---
+++ bash/init_env.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+#source config/env_config
+
+SVN_REV=$1
+
+echo "svn: "${SVN_REV}
+
+startpwd=$(pwd)
+
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+cd build
+
+if [ ! -d "conf" ]; then
+    mkdir conf
+fi
+cd conf
+
+if [ -e "local.conf.template" ]; then
+    rm -f local.conf.template
+fi
+touch local.conf.template
+
+cat ${startpwd}/config/local.conf >> local.conf.template
+sed -i "s:MACHINE\ \=.*:MACHINE\ \=\ \"TEST\":g local.conf.template
+cp local.conf.template local.conf
