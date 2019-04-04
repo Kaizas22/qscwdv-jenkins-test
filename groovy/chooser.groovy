e335456773fd5a3ac8a3b0664a3a170432a474ff
Kaizas22 <kaizas22@gmail.com>
2019-04-04 17:33:11 +0200

    Update chooser.groovy

--- groovy/chooser.groovy
+++ groovy/chooser.groovy
@@ -1,31 +1,31 @@
-def branch
+def version
 def target
 
 def chooseBranch(version) {
     switch (version) {
         case 'v1.0':
-            branch = 'v1.0'
+            version = '1.0'
             break
         case 'v1.1':
-            branch = 'v1.1'
+            version = '1.1'
             break
         case 'v1.2':
-            branch = 'v1.2'
+            version = '1.2'
             break
         case 'v2.0':
-            branch = 'v2.0'
+            version = '2.0'
             break
         case 'v2.1':
-            branch = 'v2.1'
+            version = '2.1'
             break
         default:
-            branch = 'master'
+            version = 'master'
             break
     }
 }
 
 def getBranch() {
-    return branch
+    return version
 }
 
 def chooseTarget(device) {
