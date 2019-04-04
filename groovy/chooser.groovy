95c167e79c548cf5a3ca4218ee908851bfe9fa5b
Kaizas22 <kaizas22@gmail.com>
2019-04-04 18:20:20 +0200

    Update chooser.groovy

--- groovy/chooser.groovy
+++ groovy/chooser.groovy
@@ -55,7 +55,7 @@ def chooseLinuxVersion(version) {
 
 def checkTargetVersion(target,version) {
     echo "${this.getTarget()} : ${this.getVersion()}"
-    assert this.A_VERSIONS.contains(this.getVersion)
+    assert this.A_VERSIONS.contains(this.getVersion())
     echo "true"
 }
 
