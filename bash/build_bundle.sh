fbda2b934664bc642956da3665b60aa2573d698a
Kaizas22 <kaizas22@gmail.com>
2019-04-05 18:06:37 +0200

    Update build_bundle.sh

--- bash/build_bundle.sh
+++ bash/build_bundle.sh
@@ -3,18 +3,9 @@
 pwd
 
 PLATFORM=$1
-SDKMACHINE=$2
 
 echo "source poky/oe-init-build-env \"\""
 
-echo $PLATFORMSDK
-echo $SDKMACHINE
-echo $SDKTYPE
+echo $PLATFORM
 
-echo "cd conf"
-echo "rm local.conf"
-echo "cp local.conf.template local.conf"
-echo "echo \"SDKMACHINE = \"$SDKMACHINE\"\" >> local.conf"
-echo "cd .."
-
-echo "bitbake $PLATFORMSDK -c populate_sdk"
+echo "bitbake $PLATFORM-bundle-base -c populate_sdk"
