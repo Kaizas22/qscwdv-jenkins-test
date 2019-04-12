718b79079ba38f2d8a9ab937569a3883153781be
Kaizas22 <kaizas22@gmail.com>
2019-04-13 00:58:12 +0200

    Update copy_results.sh

--- bash/copy_results.sh
+++ bash/copy_results.sh
@@ -44,21 +44,20 @@ fi
 
 copy ${build_folder}/buildhistory/images/${MACHINE}/glibc/${DEVICE}-image-base/build-id.txt ${output_folder}/version_info/
 copy ${build_folder}/buildhistory/metadata-revs ${output_folder}/version_info/
-copy ${build_folder}/tmp/work/corei7-64-pxc-linux/fw/1.1.0-r0/image/etc/plcnext/fwversion ${output_folder}/version_info/
-copy ${build_folder}/tmp/work/corei7-64-pxc-linux/fw/1.1.0-r0/image/etc/plcnext/FwVersionJenkins.properties ${output_folder}/version_info/
 copy ${deploy_folder}/images/${MACHINE}/${DEVICE}-image.base-${MACHINE}.manifest ${output_folder}/${DEVICE}-package.manifest
 
 tar -C ${build_folder}/tmp/work/corei7-64-pxc-linux/arp/1.1.0-r0/build -czf ${output_folder}/api_documentation.tar.gz html
 
 if [ "${SPECIAL_BUILD}" == "true" ]; then
     copy ${deploy_folder}/images/${MACHINE}/${DEVICE}-image-production-${MACHINE}.* ${output_folder}/usb/${DEVICE}-production-image-${svn_revision}.*
-    tar -czvf ${output_folder}/sources.tar.gz ${deploy_folder}/sources
+    tar -czf ${output_folder}/sources.tar.gz ${deploy_folder}/sources
 fi
 
 mkdir ${build_folder}"/temp"
 cd ${build_folder}"/temp"
 tar -xzf ${deploy_folder}/images/${MACHINE}/${DEVICE}-image-base-${MACHINE}.tar.gz
-cd ${build_folder}"/temp/usr/share/"
-tar -czf ${output_folder}/licenses.tar.gz common-licenses/
+copy ${build_folder}/temp/etc/plcnext/fwversion ${output_folder}/version_info/
+copy ${build_folder}/temp/etc/plcnext/FwVersionJenkins.properties ${output_folder}/version_info/
+tar -czf ${output_folder}/licenses.tar.gz ${build_folder}"/temp/usr/share/common-licenses/
 cd ${build_folder}
 rm -rf temp/
