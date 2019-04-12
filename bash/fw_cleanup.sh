8bdfdaa497761d0d7153621cf048e9e7518fdae4
Kaizas22 <kaizas22@gmail.com>
2019-04-13 01:44:38 +0200

    Update fw_cleanup.sh

--- bash/fw_cleanup.sh
+++ bash/fw_cleanup.sh
@@ -5,12 +5,12 @@ startpwd=$(pwd)
 #source bash/config/env_config
 #source poky/oe-init-build-env
 
-// remove all sources from download cache
+# remove all sources from download cache
 rm -rf ${YOCTO_DOWNLOAD_CACHE}"*svn01.com"
 rm -rf ${YOCTO_DOWNLOAD_CACHE}"svn/svn01.com/"
 
-// remove all sdks
+# remove all sdks
 rm -rf ${startpwd}/build/tmp/deploy/sdk/*
 
-// clean all sources from sstate-cache and tmp-dir
+# clean all sources from sstate-cache and tmp-dir
 echo "bitbake fw -c cleanall"
