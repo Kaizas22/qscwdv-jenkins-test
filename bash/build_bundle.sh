d52400fa2b45ccb2909a889640317703e110179c
Kaizas22 <kaizas22@gmail.com>
2019-05-06 22:22:19 +0200

    Update build_bundle.sh

--- bash/build_bundle.sh
+++ bash/build_bundle.sh
@@ -3,15 +3,21 @@
 startpwd=$(pwd)
 
 PLATFORM=$1
+DEVICE=$2
 
 #source bash/config/env_config
 #source poky/oe-init-build-env ""
 
-if [[ "${PLATFORM}" == "axcf2152" && "${LINUX_VERSION}" == "2019.0 LTS" ]]; then
-    echo "bitbake ${PLATFORM}-image-base-bundle"
+if [[  "${LINUX_VERSION}" == "2019.0 LTS" ]]; then
+    if [ "${PLATFORM}" == "axcf2152" ]; then
+        echo "bitbake ${PLATFORM}-image-base-bundle"
+    fi
+    if [ "${PLATFORM}" == "rfc480" ]; then
+        echo "bitbake ${DEVICE}-image-base-bundle"
+    fi
 fi
 
-if [[ "${PLATFORM}" == "axcf2152" && "${LINUX_VERSION}" != "2019.0 LTS" ]]; then
+if [ "${LINUX_VERSION}" != "2019.0 LTS" ]; then
     echo "bitbake ${PLATFORM}-bundle-base"
 fi
 
@@ -19,12 +25,4 @@ if [[ "${PLATFORM}" == "axcf2152" && "${LINUX_VERSION}" == "master" ]]; then
     echo "PREFERRED_PROVIDER_virtual/kernel = \"linux-rt-test\"" >> ${startpwd}/build/conf/local.conf
     echo "bitbake ${PLATFORM}-bundle-test"
     sed -i "s:PREFERRED_PROVIDER_virtual/kernel\ =.*::g" ${startpwd}/build/conf/local.conf
-fi
-
-if [[ "${PLATFORM}" == "rfc480" && "${LINUX_VERSION}" == "2019.0 LTS" ]]; then
-    echo "bitbake ${DEVICE}-image-base-bundle"
-fi
-
-if [[ "${PLATFORM}" == "rfc480" && "${LINUX_VERSION}" != "2019.0 LTS" ]]; then
-    echo "bitbake ${PLATFORM}-bundle-base"
 fi
